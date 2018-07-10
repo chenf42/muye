a71b77b68b9541c4f0498700e02b40a1aaae50c4
Feng Chen <chenf@outlook.com>
2018-07-10 20:38:37 +0800

    initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <Row>
+    <Col span="8" offset="8">
+      <Form>
+        <FormItem>
+          <Input placeholder="请输入用户名"></Input>
+        </FormItem>
+
+        <FormItem>
+          <Input placeholder="请输入密码"></Input>
+        </FormItem>
+
+        <FormItem>
+          <Button type="primary" long>登录</Button>
+        </FormItem>
+      </Form>
+    </Col>
+  </Row>
+</template>
